8aef6f1b0737b2e6232d32105ab7d8dbd83f272c
LanYu <lanyu19950316@gmail.com>
2014-11-07 10:04:59 +0800

    Initial commit

---
+++ 16.php
@@ -0,0 +1,6 @@
+<?php
+function GetMineURL($mine) {
+    $rand = base64_encode($mine * 60 . "&1&" . time() . "&" . rand(001,999) . "&" . rand(001,999) . "&Chrome 35.0.1916.153&0");
+    return "http://cdavtc.tsk.erya100.com/student/studentVideoAction!recieveNewVideoTime?videoId=" . $_GET["videoId"] . "&videoNumber=" . $_GET["videoNumber"] . "&courseId=" . $_GET["courseId"] . "&rand=" . $rand . "&ecode=" . md5("1erya_tsk" . $rand) . "&schoolCourseId=" . $_GET["schoolCourseId"];
+}
+echo '$.get("' . GetMineURL(16) . '");';
---
+++ end.php
@@ -0,0 +1,13 @@
+<?php
+$data = json_decode(file_get_contents("http://cdavtc.tsk.erya100.com/player/playerAction!getVideoUrl?videoId=" . $_GET["videoId"] . "&courseId=" . $_GET["courseId"] . "&schoolId=000343&seriNum=" . $_GET["videoNumber"]),1);
+function GetURLPro($data) {
+    $rand = base64_encode($data["data"]["totalTime"] . "&2&" . time() . "&" . rand(001,999) . "&" . rand(001,999) . "&Chrome 35.0.1916.153&0");
+    return "http://cdavtc.tsk.erya100.com/student/studentVideoAction!recieveNewVideoTime?videoId=" . $_GET["videoId"] . "&videoNumber=" . $_GET["videoNumber"] . "&courseId=" . $_GET["courseId"] . "&rand=" . $rand . "&ecode=" . md5("2erya_tsk" . $rand) . "&schoolCourseId=" . $_GET["schoolCourseId"];
+}
+echo '$.getJSON(
+    "' . GetURLPro($data) . '",
+    function(data) {
+       if (data.status == "1" || data.status == "4") window.close() ;
+    }
+);';
+echo '$.getScript("http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . '?videoId=" + document.getElementById("videoId").value + "&videoNumber=" + document.getElementById("videoNumber").value + "&courseId=" + document.getElementById("courseId").value + "&schoolCourseId=" + document.getElementById("schoolCourseId").value);';
